a05b88bfccd235ca73a85d985149ec0acb5fb2e2
tylerjrichards <tylerjrichards@gmail.com>
2021-02-06 18:32:36 -0800

    fixed typo

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -3,42 +3,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 
 st.title('Penguin Classifier')
-st.write("This app uses 6 inputs to predict the species of penguin using"
+st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
          " to get started!")
 
-penguin_file = st.file_uploader('Upload your own penguin data')
-
-if penguin_file is None:
-    rf_pickle = open('random_forest_penguin.pickle', 'rb')
-    map_pickle = open('output_penguin.pickle', 'rb')
-    rfc = pickle.load(rf_pickle)
-    unique_penguin_mapping = pickle.load(map_pickle)
-    rf_pickle.close()
-    map_pickle.close()
-else:
-    penguin_df = pd.read_csv(penguin_file)
-    penguin_df = penguin_df.dropna()
-    output = penguin_df['species']
-    features = penguin_df[['island', 'bill_length_mm', 'bill_depth_mm',
-                           'flipper_length_mm', 'body_mass_g', 'sex']]
-    features = pd.get_dummies(features)
-    output, unique_penguin_mapping = pd.factorize(output)
-
-    x_train, x_test, y_train, y_test = train_test_split(
-        features, output, test_size=.8)
-    rfc = RandomForestClassifier(random_state=15)
-    rfc.fit(x_train, y_train)
-    y_pred = rfc.predict(x_test)
-    score = round(accuracy_score(y_pred, y_test), 2)
-    st.write('We trained a Random Forest model on these data,'
-             ' it has a score of {}! Use the '
-             'inputs below to try out the model.'.format(score))
+penguin_df = pd.read_csv('penguins.csv')
+rf_pickle = open('random_forest_penguin.pickle', 'rb')
+map_pickle = open('output_penguin.pickle', 'rb')
+rfc = pickle.load(rf_pickle)
+unique_penguin_mapping = pickle.load(map_pickle)
+rf_pickle.close()
+map_pickle.close()
 
 island = st.selectbox('Penguin Island', options=[
                       'Biscoe', 'Dream', 'Torgerson'])
@@ -67,4 +44,36 @@ new_prediction = rfc.predict([[bill_length, bill_depth, flipper_length,
                                body_mass, island_biscoe, island_dream,
                                island_torgerson, sex_female, sex_male]])
 prediction_species = unique_penguin_mapping[new_prediction][0]
-st.write('We predict your penguin is of the {} species'.format(prediction_species))
+
+st.subheader("Predicting Your Penguin's Species:")
+st.write('We predict your penguin is of the {} species'
+         .format(prediction_species))
+st.write('We used a machine learning (Random Forest) model to '
+         'predict the species, the features used in this prediction '
+         ' are ranked by relative importance below.')
+st.image('feature_importance.png')
+
+st.write('Below are the histograms for each continuous variable '
+         'separated by penguin species. The vertical line '
+         'represents your the inputted value.')
+
+fig, ax = plt.subplots()
+ax = sns.displot(x=penguin_df['bill_length_mm'],
+                 hue=penguin_df['species'])
+plt.axvline(bill_length)
+plt.title('Bill Length by Species')
+st.pyplot(ax)
+
+fig, ax = plt.subplots()
+ax = sns.displot(x=penguin_df['bill_depth_mm'],
+                 hue=penguin_df['species'])
+plt.axvline(bill_depth)
+plt.title('Bill Depth by Species')
+st.pyplot(ax)
+
+fig, ax = plt.subplots()
+ax = sns.displot(x=penguin_df['flipper_length_mm'],
+                 hue=penguin_df['species'])
+plt.axvline(flipper_length)
+plt.title('Flipper Length by Species')
+st.pyplot(ax)
